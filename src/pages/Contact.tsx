4cf3023a58674f5c16811f408187bea413473f7e
Abdul Vahap Caliskan <vahap.caliskan@proton.me>
2025-10-04 22:58:40 +0200

    chore: fixed map in contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -105,7 +105,7 @@ const Contact = () => {
             </h2>
             <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-[var(--shadow-soft)]">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.408941678991!2d13.404953999999999!3d52.520008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851c655f20989%3A0x26bbfb4e84674c63!2sBrandenburger%20Tor!5e0!3m2!1sde!2sde!4v1234567890123!5m2!1sde!2sde"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.238393697615!2d7.247795176631875!3d50.54436297244685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf8471b32e06a5%3A0x8d6bdebb0dbab3a4!2sAusdorferstra%C3%9Fe%201A%2C%2053489%20Sinzig!5e0!3m2!1sde!2sde!4v1728047700000!5m2!1sde!2sde"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
